e59f67c1aa56bd54e825c5a3559e28c41a4b910d
95classic <ehdeld12@gmail.com>
2023-06-06 16:13:39 +0900

    tserrrrr

---
+++ Testtttttttttt/src/main/java/edu/spring/test/Test01.java
@@ -0,0 +1,12 @@
+package edu.spring.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int i = 11;
+		
+		System.out.println(i);
+
+	}
+
+}
---
+++ Testtttttttttt/src/main/java/edu/spring/test/Test2.java
@@ -0,0 +1,14 @@
+package edu.spring.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		char a = 'A';
+		
+		char b = 1;
+		
+		System.out.println(b);
+
+	}
+
+}
